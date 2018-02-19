a85ad97f17b4a0c028449d4a2444c57057e3412b
Marijn Bitter <marijn_bitter@goeiezaak.com>
2018-02-19 15:58:57 +0100

    Rebuilding to SVG (because why not)

--- js/base.js
+++ js/base.js
@@ -49,7 +49,14 @@ $(document).ready(function() {
 //=============== Canvas functions ===============//
 //Code for keeping track of objects
 canvasObjs = {
-    "mapSettings":{},
+    "mapSettings":{
+        "grid":{
+            "size":64,
+            "offsetX":0,
+            "offsetY":0,
+            "color":"#000000"
+        }
+    },
     "paintObjects":{
         "mapImage": {"src": "../images/lmop4.jpg"}
     }
@@ -58,32 +65,37 @@ canvasObjs = {
 //Code for keeping track of canvas state
 //Code for mouse events
 //Code for drawing the objects as they are made and move around
-var canvas = document.getElementById('canvas'),
-    context = canvas.getContext('2d');
+var canvas = document.getElementById('canvas');
+var ctx = canvas.getContext('2d');
 
-// resize the canvas to fill browser window dynamically
-//window.addEventListener('resize', resizeCanvas, false);
-function resizeCanvas() {
-    canvas.width = window.innerWidth;
-    canvas.height = window.innerHeight;
-
-    /**
-             * Your drawings need to be inside this function otherwise they will be reset when 
-             * you resize the browser window and the canvas goes will be cleared.
-             */
-    drawStuff(); 
+// Paint map image
+var mapImage = new Image;
+mapImage.src = canvasObjs.paintObjects.mapImage.src;
+ctx.canvas.width  = mapImage.width;
+ctx.canvas.height  = mapImage.height;
+mapImage.onload = function(){
+    ctx.drawImage(mapImage, 0, 0);
+    drawGrid();
 }
-resizeCanvas();
 
-function drawStuff() {
-    var c = document.getElementById("canvas");
-    var ctx = c.getContext("2d");
+function drawGrid(){
+    var yGridPos = 0;
+    yGridPos += canvasObjs.mapSettings.grid.offsetY;
 
-    // Paint map image
-    var mapImage = new Image;
-    mapImage.src = canvasObjs.paintObjects.mapImage.src;
-    mapImage.onload = function(){
-        ctx.drawImage(mapImage, 0, 0);
+    while(yGridPos < ctx.canvas.height) {
+        var xGridPos = 0;
+        xGridPos += canvasObjs.mapSettings.grid.offsetX;
+
+        while(xGridPos < ctx.canvas.width) {
+            ctx.beginPath();
+            ctx.lineWidth="2";
+            ctx.strokeStyle=canvasObjs.mapSettings.grid.color;
+            ctx.rect(xGridPos, yGridPos, canvasObjs.mapSettings.grid.size, canvasObjs.mapSettings.grid.size); 
+            ctx.stroke(); 
+
+            xGridPos += canvasObjs.mapSettings.grid.size;
+        }
+        yGridPos += canvasObjs.mapSettings.grid.size;
     }
 }
 
